6711665d18a1c58d1a9afe347f419a8bc600a859
Cooper Cain <coopercc@uw.edu>
2016-10-26 15:11:38 -0700

    Some hash methods added

--- CSE-373-A3-Hashing/src/ColorHash.java
+++ CSE-373-A3-Hashing/src/ColorHash.java
@@ -1,3 +1,5 @@
+
+
 /**
  * @author (put your name here)
  *
@@ -7,17 +9,59 @@ public class ColorHash {
 	// of the public methods below.  You may create any number of private methods
 	// if you find them to be helpful.
 	
-	public ColorHash(int tableSize, int bitsPerPixel, String collisionResolutionMethod, double rehashLoadFactor){}
+	ColorKey[] keyArr;
+	long[] valueArr;
+	String resMethod; //"Linear Probing" and "Quadratic Probing"
+	int bpp;
+	double rehashFactor;
+	int currElements;
+	int collisions;
 	
-	public ResponseItem colorHashPut(ColorKey key, long value){return null;}
-	public ResponseItem increment(ColorKey key){return null;}
+	
+	public ColorHash(int tableSize, int bitsPerPixel, String collisionResolutionMethod, double rehashLoadFactor){
+		keyArr = new ColorKey[tableSize];
+		valueArr = new long[tableSize];
+		bpp = bitsPerPixel;
+		resMethod = collisionResolutionMethod;
+		rehashFactor = rehashLoadFactor;
+		currElements = 0;
+	}
+	
+	public ResponseItem colorHashPut(ColorKey key, long value) {
+		//place at key.HashCode % tableSize
+		//increment currElements if you don't switch to increment if exists, increment, else do thing and c
+		long pos = key.hashCode() % getTableSize();
+		return null;
+	}
+	
+	public ResponseItem increment(ColorKey key) {
+		return null;
+		//getCount(key)
+	}
 	public ResponseItem colorHashGet(ColorKey key)  throws Exception{return null;}
-	public long getCount(ColorKey key){return -1L;}
-	public ColorKey getKeyAt(long tableIndex){return null;}
-	public long getValueAt(long tableIndex){return -1L;}
-	public double getLoadFactor(){return -1.0;}
-	public int getTableSize(){return -1;}
-	public void resize(){}
+	
+	public long getCount(ColorKey key) {
+		return getValueAt(Arrays.asList(keyArr).indexOf(key));
+	}
+	
+	public ColorKey getKeyAt(long tableIndex) {
+		return keyArr[tableIndex];
+	}
+	
+	public long getValueAt(long tableIndex) {
+		return valueArr[tableIndex];
+	}
+	
+	public double getLoadFactor() {
+		return (double)currElements / getTableSize();
+	}
+	
+	public int getTableSize() {
+		return keyArr.length;
+	}
+	public void resize() {
+		
+	}
 
 
 }
