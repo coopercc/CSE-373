c40d4ad9d9908f22af5579c2071a819ab0a37e57
Cooper Cain <coopercc@uw.edu>
2016-10-30 11:28:03 -0700

    A3 finished, ready for A4

---
+++ CSE-373-A3-Hashing/src/FindObject.java
@@ -0,0 +1,11 @@
+
+//used to return an index and whether or not there is a key at that index.
+public class FindObject {
+	public int index;
+	public boolean inTable;
+	
+	public FindObject(int index, boolean inTable) {
+		this.index = index;
+		this.inTable = inTable;
+	}
+}
---
+++ CSE-373-A3-Hashing/src/HashEntry.java
@@ -0,0 +1,13 @@
+
+//holds the key and value in the hash table
+public class HashEntry
+{
+	public ColorKey key;
+    public long value;
+
+    HashEntry(ColorKey key, long value) {
+          this.key = key;
+          this.value = value;
+    }     
+
+}
